694ee7186376af4519f7c2d27cfdcc22ccdc92e1
unknown <lair60@yahoo.es>
2021-02-07 16:28:35 +0000

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,9 +27,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 	// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    ),
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
     BrowserModule,
     AppRoutingModule,
 	FormsModule
